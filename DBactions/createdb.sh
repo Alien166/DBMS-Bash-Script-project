98a56ab3d53d496684fa82d97b8bf0239e4926f9
Alien166 <abdowagieh@gmail.com>
2024-08-26 11:37:14 +0300

    lastest

--- DBactions/createdb.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-if [[ -d data/$1 ]]; then
-		echo "The DataBase $1 already exists";
-else
-	mkdir  data/$1
-fi  
--- DBactions/renamedb.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-echo "Available DataBases"
-ls -1 data/
-
-read -p " Enter the DataBase :" $dbname
-read -p " Enter the New Name :" $dbnew
-mv /data/$dbName  /data/$dbnew
